d46bd792b3c1a72a239e63d9dc00a5014cda2ee0
Mark McDonald <pair+mark.mcdonald.ga@gmail.com>
2015-07-04 18:36:33 -0400

    Start by looking up all runes / champions, then fall back to individual calls if some missing

--- lib/champion_fetcher.rb
+++ lib/champion_fetcher.rb
@@ -4,13 +4,21 @@ require 'json'
 
 class ChampionFetcher
   def fetch(champion_id)
-    @@champions ||= {}
-
+    @@champions ||= all_champions
     return @@champions[champion_id] if @@champions[champion_id]
 
     champion_info_endpoint = "#{LolApiConfig::LOL_API_BASE_URL}/api/lol/static-data/#{LolApiConfig::LOL_API_REGION}/v1.2/champion/#{champion_id}?api_key=#{ENV['LOL_API_KEY']}"
     champion_info_response = RestClient.get(champion_info_endpoint)
     @@champions[champion_id] = JSON.parse(champion_info_response)
+  end
+
+  private
+  def all_champions
+    all_champions = JSON.parse(RestClient.get("#{LolApiConfig::LOL_API_BASE_URL}/api/lol/static-data/#{LolApiConfig::LOL_API_REGION}/v1.2/champion?api_key=#{ENV['LOL_API_KEY']}"))
+    all_champions['data'].reduce({}) do |champions, (id, champion)|
+      champions[id] = champion
+      champions
+    end
   end
 
 end
--- lib/rune_lookup.rb
+++ lib/rune_lookup.rb
@@ -1,3 +1,4 @@
+require 'lol_api_config'
 require 'rest_client'
 require 'json'
 
@@ -6,7 +7,7 @@ class RuneLookup
   Rune = Struct.new(:id, :name, :type)
 
   def self.by_id(id)
-    @@runes ||= {}
+    @@runes ||= self.all_runes
     return @@runes[id] if @@runes[id]
 
     api_key = ENV['LOL_API_KEY']
@@ -18,5 +19,14 @@ class RuneLookup
     rune_info = JSON.parse(rune_info_response)
 
     @@runes[id] = Rune.new(id.to_i, rune_info['name'], rune_info['rune']['type'])
+  end
+
+  private
+  def self.all_runes
+    all_runes = JSON.parse(RestClient.get("#{LolApiConfig::LOL_API_BASE_URL}/api/lol/static-data/#{LolApiConfig::LOL_API_REGION}/v1.2/rune?runeListData=colloq,depth&api_key=#{ENV['LOL_API_KEY']}"))
+    all_runes['data'].reduce({}) do |runes, (id, rune)|
+      runes[id] = Rune.new(id.to_i, rune['name'], rune['rune']['type'])
+      runes
+    end
   end
 end
