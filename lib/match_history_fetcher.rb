7f4320ce6ec285f616826171db297638565773d7
Mark McDonald <pair+mark.mcdonald.ga@gmail.com>
2015-07-05 14:47:31 -0400

    Handle failures to look up summoner or match history

--- lib/match_history_fetcher.rb
+++ lib/match_history_fetcher.rb
@@ -7,12 +7,17 @@ class MatchHistoryFetcher
   def fetch(summoner_name, region)
     summoner_name = summoner_name.downcase.delete(' ')
 
-    summoner_id_endpoint = "#{LolApiConfig.base_url(region)}/api/lol/#{region}/v1.4/summoner/by-name/#{URI.escape(summoner_name)}?api_key=#{ENV['LOL_API_KEY']}"
-    summoner_id_response = RestClient.get(summoner_id_endpoint)
-    summoner_id = JSON.parse(summoner_id_response)[summoner_name]['id']
+    begin
+      summoner_id_endpoint = "#{LolApiConfig.base_url(region)}/api/lol/#{region}/v1.4/summoner/by-name/#{URI.escape(summoner_name)}?api_key=#{ENV['LOL_API_KEY']}"
+      summoner_id_response = RestClient.get(summoner_id_endpoint)
+      summoner_id = JSON.parse(summoner_id_response)[summoner_name]['id']
 
-    match_history_endpoint = "#{LolApiConfig.base_url(region)}/api/lol/#{region}/v2.2/matchhistory/#{summoner_id}?api_key=#{ENV['LOL_API_KEY']}"
-    match_history_response = RestClient.get(match_history_endpoint)
+      match_history_endpoint = "#{LolApiConfig.base_url(region)}/api/lol/#{region}/v2.2/matchhistory/#{summoner_id}?api_key=#{ENV['LOL_API_KEY']}"
+      match_history_response = RestClient.get(match_history_endpoint)
+    rescue RestClient::Exception
+      return []
+    end
+
     JSON.parse(match_history_response)['matches']
   end
 end
